c51d0464b121a6c3b7ce6bef293fb6d2471b6f5a
andresvg8 <66088651+andresvg8@users.noreply.github.com>
2021-01-05 13:41:41 -0500

    Initial commit

---
+++ src/main/java/com/mitocode/DemoWeb3Application.java
@@ -0,0 +1,13 @@
+package com.mitocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWeb3Application.class, args);
+	}
+
+}
